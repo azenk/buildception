0e52662f669ef82d66df792028a456e8780c0f76
Andrew Zenk <azenk@umn.edu>
2014-08-28 00:06:59 -0500

    meh

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
 
-def change_sources():
-	from buildbot.changes.gitpoller import GitPoller
-	srcs = []
-	srcs.append(GitPoller(
+from buildbot.changes.gitpoller import GitPoller
+changes = []
+changes.append(GitPoller(
         	'https://github.com/azenk/smoker-api.git',
         	workdir='smokerapi-workdir', branch='master',
         	pollinterval=30))
-	return srcs
