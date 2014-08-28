5d484dfc1da2b5d07f0460b82bb9903f27c2a197
Andrew Zenk <azenk@umn.edu>
2014-08-27 22:35:24 -0500

    Added test change source

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
 def config(c):
+	c['change_source'].append(GitPoller(
+        	'https://github.com/azenk/smoker-api.git',
+        	workdir='smokerapi-workdir', branch='master',
+        	pollinterval=30))
 	pass
