430a2c3bac46b466f245a609a2795189f06e24e5
Andrew Zenk <azenk@umn.edu>
2014-08-28 00:07:57 -0500

    another git poller

--- __init__.py
+++ __init__.py
@@ -2,6 +2,10 @@
 from buildbot.changes.gitpoller import GitPoller
 changes = []
 changes.append(GitPoller(
+        	'https://github.com/azenk/smoker-client.git',
+        	workdir='smokerclient-workdir', branch='master',
+        	pollinterval=30))
+changes.append(GitPoller(
         	'https://github.com/azenk/smoker-api.git',
         	workdir='smokerapi-workdir', branch='master',
         	pollinterval=30))
